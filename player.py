2980ac1d3b636f14b3ec364c0b65cb67507f9118
jek <idc@rbk.ca>
2020-12-14 16:17:48 -0800

    update movement and hitbox

--- player.py
+++ player.py
@@ -9,38 +9,59 @@ class Player:
     def __init__(self):
         ss = pg.image.load(ss_path)
         ss_w, ss_h = ss.get_size()
-        f_w, f_h = ss_w//alen, ss_h//alen
+        self.f_w, self.f_h = ss_w//alen, ss_h//alen
 
         self.animations = {
-            MF : get_quad_row(ss, ss_w,     0, f_h, alen),
-            MB : get_quad_row(ss, ss_w,   f_h, f_h, alen),
-            MR : get_quad_row(ss, ss_w, 2*f_h, f_h, alen),
-            ML : get_quad_row(ss, ss_w, 3*f_h, f_h, alen),
-            IF : alen * [ get_quad(ss, (0, 0), f_w, f_h) ],
-            IB : alen * [ get_quad(ss, (0, f_h), f_w, f_h) ],
-            IR : alen * [ get_quad(ss, (0, 2 * f_h), f_w, f_h) ],
-            IL : alen * [ get_quad(ss, (3 * f_w, 3 * f_h), f_w, f_h) ]
+            MF : get_quad_row(ss, ss_w,            0, self.f_h, alen),
+            MB : get_quad_row(ss, ss_w,     self.f_h, self.f_h, alen),
+            MR : get_quad_row(ss, ss_w, 2 * self.f_h, self.f_h, alen),
+            ML : get_quad_row(ss, ss_w, 3 * self.f_h, self.f_h, alen),
+            IF : alen * [ get_quad(ss, (0,                       0), self.f_w, self.f_h) ],
+            IB : alen * [ get_quad(ss, (0,                self.f_h), self.f_w, self.f_h) ],
+            IR : alen * [ get_quad(ss, (0,            2 * self.f_h), self.f_w, self.f_h) ],
+            IL : alen * [ get_quad(ss, (3 * self.f_w, 3 * self.f_h), self.f_w, self.f_h) ]
         }
 
-        self.aidx, self.fps, self.timer = 0, 5, 0
+        self.x, self.y = 150, 150
+        self.movespeed = 300
+
+        self.fps = self.movespeed/30
+        self.aidx, self.timer = 0, 0
         self.state = IF
 
-        self.x, self.y = 200, 200
+        self.hitbox = self.recalc_hitbox()
     
     def on_keyup(self, k, 
         ku_xsitions = {pg.K_a:IR, pg.K_s:IF, pg.K_d:IL, pg.K_w:IB}):
         if k in ku_xsitions: self.state = ku_xsitions[k]
 
+    def recalc_hitbox(self):
+        return pg.Rect( self.x, 
+                        self.y + 2/3 * self.f_h, 
+                        self.f_w, 
+                        1/3 * self.f_h)
+
     def update(self, dt):
 
+        dx, dy = 0, 0 
         keymap = pg.key.get_pressed()
-        if keymap[pg.K_a]:   self.state = MR
-        elif keymap[pg.K_s]: self.state = MF
-        elif keymap[pg.K_d]: self.state = ML
-        elif keymap[pg.K_w]: self.state = MB
+        if keymap[pg.K_a]:   
+            self.state = MR
+            dx = -1*self.movespeed
+        elif keymap[pg.K_s]: 
+            self.state = MF
+            dy = self.movespeed
+        elif keymap[pg.K_d]: 
+            self.state = ML
+            dx = self.movespeed
+        elif keymap[pg.K_w]: 
+            self.state = MB
+            dy = -1*self.movespeed
 
-        if self.state in (MR, MF, ML, MB):
-            # code for moving and collision checking
+        self.x += dx * dt
+        self.y += dy * dt
+
+        self.hitbox = self.recalc_hitbox()
 
         self.timer += dt
         if self.timer >= 1/self.fps:
@@ -48,4 +69,5 @@ class Player:
             self.aidx = (self.aidx + 1) % alen
 
     def draw(self, surf):
+        pg.draw.rect(surf, (255, 255, 0), self.hitbox, 1)
         surf.blit(self.animations[self.state][self.aidx], (self.x, self.y))
