2e70c8d611ec6f1193a706fddfb5d2f2a9982196
jryzkns <jryzkns@gmail.com>
2020-12-20 21:26:25 -0800

    implement map prerendering

--- tilemap.py
+++ tilemap.py
@@ -19,13 +19,20 @@ class Tilemap:
             for i in range(len(self.map)):
                 self.map[i] = [int(val) for val in self.map[i].split(',')]
 
-    def update(self, dt):
-        pass
+        self.pre_render_map = pg.Surface(res)
+        print(self.pre_render_map)
 
-    # TODO: try to move drawing tile by tile to constructor
-    # use the idea with stationary platforms from platformer
-    def draw(self, surf):
+        self.prerender()
+
+        
+    def prerender(self):
         for i in range(res[0]//self.tile_length):
             for j in range(res[1]//self.tile_length):
                 loc = (i * self.tile_length, j * self.tile_length)
-                surf.blit(self.tiles[self.map[j][i]], loc)
+                self.pre_render_map.blit(self.tiles[self.map[j][i]], loc)
+
+    def update(self, dt):
+        pass
+
+    def draw(self, surf):
+        surf.blit(self.pre_render_map, (0,0))
